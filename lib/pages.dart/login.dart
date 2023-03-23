bf55be822f46cb071f088745bfaaa4e3201778f9
AbulrhamnMohamed <126196441+AbulrhamnMohamed@users.noreply.github.com>
2023-03-23 15:19:22 +0200

    add obscure text and take a value 'false'

--- lib/pages.dart/login.dart
+++ lib/pages.dart/login.dart
@@ -11,6 +11,7 @@ class Login extends StatelessWidget {
           children: [
             TextField(
               keyboardType: TextInputType.emailAddress,
+              obscureText: false,
             ),
           ],
         ),
