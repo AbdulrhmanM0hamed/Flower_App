91bca446386f9468985ae0dfd4504f8ccd130e2e
AbulrhamnMohamed <126196441+AbulrhamnMohamed@users.noreply.github.com>
2023-03-23 15:39:58 +0200

    repetition an text field

--- lib/pages.dart/login.dart
+++ lib/pages.dart/login.dart
@@ -26,6 +26,23 @@ class Login extends StatelessWidget {
                     filled: true,
                     contentPadding: const EdgeInsets.all(8)),
               ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                obscureText: false,
+                decoration: InputDecoration(
+                    hintText: "Enter your Email :",
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: Divider.createBorderSide(context),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    filled: true,
+                    contentPadding: const EdgeInsets.all(8)),
+              ),
             ],
           ),
         ),
