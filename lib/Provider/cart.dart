a1cd84e16c5619ff87e999aebb2e8ee70182a456
AbulrhamnMohamed <126196441+AbulrhamnMohamed@users.noreply.github.com>
2023-03-28 21:51:44 +0200

    call up the Home page

--- lib/Provider/cart.dart
+++ lib/Provider/cart.dart
@@ -1,9 +1,12 @@
+import 'package:flower_app/pages/Home.dart';
+import 'package:flower_app/pages/Home.dart';
+
 import 'package:flutter/material.dart';
 
 class Cart with ChangeNotifier {
   List selectedproduct = [];
 
-  add() {
+  add(Item.product) {
     selectedproduct.add(product);
   }
 }
