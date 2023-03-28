1d44614550113663737eda44f41333784fda4d7a
AbulrhamnMohamed <126196441+AbulrhamnMohamed@users.noreply.github.com>
2023-03-28 02:22:40 +0200

    wrap the matrial app in widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Homepage(),
+    return ChangeNotifierProvider(
+      create: (context) (return ClassName();),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Homepage(),
+      ),
     );
   }
 }
