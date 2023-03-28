6e1fd69bb1e3c3fd8fea69b6666744e7df06fc5d
AbulrhamnMohamed <126196441+AbulrhamnMohamed@users.noreply.github.com>
2023-03-28 22:08:43 +0200

    un comment class in cart page

--- lib/Provider/cart.dart
+++ lib/Provider/cart.dart
@@ -1,12 +1,12 @@
 import 'package:flower_app/pages/Home.dart';
-// import 'package:flower_app/pages/Home.dart';
+import 'package:flower_app/pages/Home.dart';
 
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class Cart with ChangeNotifier {
-//   List selectedproduct = [];
+class Cart with ChangeNotifier {
+  List selectedproduct = [];
 
-//   add(Item.product) {
-//     selectedproduct.add(product);
-//   }
-// }
+  add(Item.product) {
+    selectedproduct.add(product);
+  }
+}
